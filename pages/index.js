4cdcd9f8e1370e55302eab594e86fa662b870689
Duck1s <joeri.breedveld123@gmail.com>
2022-07-28 14:04:41 +0200

    linking between pages

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from "next/image"
 import Footer from "../comps/Footer"
 import Navbar from "../comps/Navbar"
 import styles from "../styles/Home.module.css"
+import Link from "next/link"
 
 export default function Home() {
 	return (
@@ -17,6 +18,9 @@ export default function Home() {
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis perferendis quae obcaecati molestiae nam eligendi facilis aperiam at aliquid qui sit cumque vero non laborum iure,
 				consequatur voluptatum veritatis totam!
 			</p>
+			<Link href='/ninjas'>
+				<a>See Ninja Listing</a>
+			</Link>
 			<Footer />
 		</div>
 	)
