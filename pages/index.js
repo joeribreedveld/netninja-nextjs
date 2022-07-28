1d3d02e7d0785884278170432d606d02687bba7b
Duck1s <joeri.breedveld123@gmail.com>
2022-07-28 14:24:46 +0200

    adding styles

--- pages/index.js
+++ pages/index.js
@@ -8,17 +8,17 @@ import Link from "next/link"
 export default function Home() {
 	return (
 		<div>
-			<h1>Homepage</h1>
-			<p>
+			<h1 className={styles.title}>Homepage</h1>
+			<p className={styles.text}>
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis perferendis quae obcaecati molestiae nam eligendi facilis aperiam at aliquid qui sit cumque vero non laborum iure,
 				consequatur voluptatum veritatis totam!
 			</p>
-			<p>
+			<p className={styles.text}>
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis perferendis quae obcaecati molestiae nam eligendi facilis aperiam at aliquid qui sit cumque vero non laborum iure,
 				consequatur voluptatum veritatis totam!
 			</p>
 			<Link href='/ninjas'>
-				<a>See Ninja Listing</a>
+				<a className={styles.btn}>See Ninja Listing</a>
 			</Link>
 		</div>
 	)
