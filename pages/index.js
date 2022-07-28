bd30aae179fd5a788c3b82e0e3ce1a67b3dde725
Duck1s <joeri.breedveld123@gmail.com>
2022-07-28 13:57:15 +0200

    adding other components

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Head from "next/head"
 import Image from "next/image"
+import Footer from "../comps/Footer"
+import Navbar from "../comps/Navbar"
 import styles from "../styles/Home.module.css"
 
 export default function Home() {
 	return (
 		<div>
+			<Navbar />
 			<h1>Homepage</h1>
 			<p>
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis perferendis quae obcaecati molestiae nam eligendi facilis aperiam at aliquid qui sit cumque vero non laborum iure,
@@ -14,6 +17,7 @@ export default function Home() {
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis perferendis quae obcaecati molestiae nam eligendi facilis aperiam at aliquid qui sit cumque vero non laborum iure,
 				consequatur voluptatum veritatis totam!
 			</p>
+			<Footer />
 		</div>
 	)
 }
