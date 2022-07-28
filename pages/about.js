9e549e787f6e7d3f9c09483c89a4065f256708e1
Duck1s <joeri.breedveld123@gmail.com>
2022-07-28 13:52:32 +0200

    pages and routes

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,7 @@
-import Head from "next/head"
-import Image from "next/image"
-import styles from "../styles/Home.module.css"
-
-export default function Home() {
+const About = () => {
 	return (
 		<div>
-			<h1>Homepage</h1>
+			<h1>About</h1>
 			<p>
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis perferendis quae obcaecati molestiae nam eligendi facilis aperiam at aliquid qui sit cumque vero non laborum iure,
 				consequatur voluptatum veritatis totam!
@@ -17,3 +13,5 @@ export default function Home() {
 		</div>
 	)
 }
+
+export default About
